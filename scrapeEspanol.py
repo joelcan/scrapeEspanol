61fa3bfe9397059bc1e16ff17f2315b0bb1d6460
SimonJester <joelc.usa@gmail.com>
2016-11-01 12:52:09 -0600

    scrape all other conjugations

--- scrapeEspanol.py
+++ scrapeEspanol.py
@@ -9,6 +9,7 @@ from __future__ import print_function #This *must* be the first line
 import sys
 import urllib2
 from bs4 import BeautifulSoup
+import re
 
 #TODO: Handle when infinitive is not found.
 #TODO: Handle when not all conjugations are listed.
@@ -22,6 +23,11 @@ def cleanText(string):
         string = string[:-1]
     return string
 
+def removeTags(string):
+    cleanre = re.compile('<.*?>')
+    cleanString = re.sub(cleanre, '', string)
+    return cleanString
+
 def scrapeWebpage(url):
     page = urllib2.urlopen(url)
     soup = BeautifulSoup(page, "html.parser")
@@ -31,7 +37,12 @@ def scrapeWebpage(url):
 
     participle = cleanText(soup.find(text='Participle:').findNext('span').string)
     print(participle)
+    print('---')
 
+    items = soup.findAll('td', {'class': 'vtable-word'})
+    for item in items[:60]:
+        word = removeTags(str(item))
+        print(word)
 
 
 def displayUsage():
